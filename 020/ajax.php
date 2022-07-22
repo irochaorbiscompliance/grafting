c8d42cfdc421d8fa55dd7a837ea1076267360a80
Israel Rocha <53264974+irochaorbiscompliance@users.noreply.github.com>
2022-07-22 15:52:41 -0400

    data

---
+++ 020/ajax.php
@@ -0,0 +1,7 @@
+<?php 
+echo 'POR GET: ' . var_dump($_GET);
+echo '<br>';
+echo 'POR POST: ' . var_dump($_POST);
+echo '<br>';
+echo 'POR FILES: ';
+var_dump($_FILES) . '<br>';
---
+++ 022/ajax.php
@@ -0,0 +1,12 @@
+<?php 
+
+var_dump($_POST['usuario']);
+
+echo 'POR GET';
+var_dump($_GET);
+
+echo 'POR POST';
+var_dump($_POST);
+
+echo 'POR FILES';
+var_dump($_FILES);
